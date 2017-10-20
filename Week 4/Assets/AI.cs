cf681ef6c2b84098a203e390f920593f3b8fb0a7
Sethiel Cosio <jann_cosio@yahoo.com>
2017-10-20 19:53:26 +0400

    My week 4 commit. Terrain and Nav Mesh lessons are in here

---
+++ Week 4/Assets/AI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    [SerializeField]
+    Transform Destination;
+
+    [SerializeField]
+    NavMeshAgent myUnit;
+    // Use this for initialization
+    void Start()
+    {
+        myUnit = GetComponent<NavMeshAgent>();
+        myUnit.SetDestination(Destination.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
